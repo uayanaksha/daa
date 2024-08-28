63c071641be1a6d00ca2de7543489a938565ddc6
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2024-08-28 11:50:22 +0530

    added: insertion sort
    main func, Err handled, display output

---
+++ insertionsort.c
@@ -0,0 +1,60 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned insertion_sort(int array[], unsigned size){
+    unsigned comparisons = 0;
+    for(int i = 1; i < size; i++){
+        comparisons++;
+        int key = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > key ) {
+            comparisons++;
+            array[j+1] = array[j];
+            j--;
+        }   j++;
+        array[j] = key;
+    }   return comparisons;
+}
+
+void get(int array[], unsigned size){
+    if(size < 1) return;
+    printf("Array[%u]: [", size);
+    for (int i = 0; i < size; i++) {
+        printf(" %d,", array[i]);
+    }   printf("];\n");
+}
+
+int main(void){
+    // Get  size of array
+    unsigned size;
+    printf("Enter size of array: ");
+    int err = scanf("%u", &size);
+    if(err != 1){
+        fprintf(stderr, "Error: Invalid input!\n");
+        return 1;
+    }
+    if(size < 0 || size > 256){
+        fprintf(stderr, "Warning: Beyond input range [0~256]!\n");
+        return 1;
+    }
+
+    // Populate array
+    srand(time(0));
+    const int low = 150;
+    const int high = 450;
+    int *array = calloc(size, sizeof(int));
+    for (int i = 0; i < size; i++) {
+        int element = low + rand() % (high - low + 1);
+        array[i] = element;
+    }   get(array, size);
+
+    // sort
+    const int comparisons = insertion_sort(array, size);
+    get(array, size);
+    printf("Total number of comparisons: %u\n", comparisons);
+
+    // Delete array
+    free(array);
+    return 0;
+}
