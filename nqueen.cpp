db967ad2f46dd30ef7a44008d95e4bc2cb34e829
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2024-08-31 02:16:21 +0530

    added: NQueen dp/tabulation impl
    [tried & tested](Sat Aug 31 02:16:21 AM IST 2024)

---
+++ nqueen.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+class Board {
+    private:
+        vector<vector<int>> board;
+        unsigned n;
+    public:
+        Board(const unsigned &n) {
+            this->n = n;
+            board = vector<vector<int>>(n, vector<int>(n, 0));
+        }
+
+        void print(){
+            cout << "Board[" << n << "x" << n << "]: {" << endl;
+            for(vector<int> rows : board){
+                cout << "    ";
+                for(int cell : rows){
+                    cout << " " << (cell == 0 ? "." : "Q");
+                }   cout << endl;
+            }   cout << "}" << endl;
+        }
+
+        void solveNQ(){
+            vector<int> ld((2*n - 1), 0);
+            vector<int> rd((2*n - 1), 0);
+            vector<int> cl(n, 0);
+            if(solveNQ_priv(0, ld, rd, cl)) cout << "Nqueen solved" << endl;
+            else cout << "Cannot be solved" << endl;
+        }
+        
+    private:
+        bool solveNQ_priv(int col, vector<int> &ld, vector<int> &rd, vector<int> &cl){
+            if(col >= n) return true;
+            int ldIdx, rdIdx;
+            for (int row = 0; row < n; row++) {
+                ldIdx = (n-1) + (row-col);
+                rdIdx = row + col;
+                if(ld[ldIdx] != 1 && rd[rdIdx] != 1 && cl[row] != 1){
+                    board[row][col] = ld[ldIdx] = rd[rdIdx] = cl[row] = 1;
+                    if(solveNQ_priv(col + 1, ld, rd, cl)) return true;
+                    board[row][col] = ld[ldIdx] = rd[rdIdx] = cl[row] = 0;
+                }
+            }   return false;
+        }
+};
+
+int main(void){
+    unsigned n;
+    cout << "Enter N queen: " && cin >> n;
+    Board board(n);
+    board.print();
+    board.solveNQ();
+    board.print();
+}
